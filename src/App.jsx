cbb7aea177bb3a9885e83b8daafd6b6246b47533
kirlosbasta <140125591+kirlosbasta@users.noreply.github.com>
2025-08-03 21:43:30 +0000

    desktop desgin

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import Input from './components/Input';
-import Label from './components/Label';
-import Radio from './components/Radio';
-import Success from './components/Success';
-import Error from './components/Error';
+import { useState } from "react";
+import Input from "./components/Input";
+import Label from "./components/Label";
+import Radio from "./components/Radio";
+import Success from "./components/Success";
+import Error from "./components/Error";
 
 function App() {
   const [success, setSuccess] = useState(false);
   const [data, setData] = useState({
-    fname: { value: '', error: '' },
-    lname: { value: '', error: '' },
-    email: { value: '', error: '' },
-    qType: { value: '', error: '' },
-    message: { value: '', error: '' },
-    consent: { value: false, error: '' },
+    fname: { value: "", error: "" },
+    lname: { value: "", error: "" },
+    email: { value: "", error: "" },
+    qType: { value: "", error: "" },
+    message: { value: "", error: "" },
+    consent: { value: false, error: "" },
   });
 
   function handleFormChange(name, value) {
@@ -25,7 +25,7 @@ function App() {
         [name]: { value, error },
       });
     } else {
-      setData({ ...data, [name]: { error: '', value } });
+      setData({ ...data, [name]: { error: "", value } });
     }
   }
 
@@ -37,21 +37,21 @@ function App() {
    * @returns {String} Empty string if valid or Error message
    */
   function getError(name, value) {
-    let error = '';
+    let error = "";
 
     switch (name) {
-      case 'consent':
+      case "consent":
         error =
-          !value && 'To submit this form, please consent to being contacted';
+          !value && "To submit this form, please consent to being contacted";
         break;
-      case 'email':
-        error = !isEmail(value) && 'Please enter a valid email address';
+      case "email":
+        error = !isEmail(value) && "Please enter a valid email address";
         break;
-      case 'qType':
-        error = !value && 'Please select a query type';
+      case "qType":
+        error = !value && "Please select a query type";
         break;
       default:
-        error = !value && 'This field is required';
+        error = !value && "This field is required";
         break;
     }
 
@@ -67,7 +67,7 @@ function App() {
     // check each field if it's valid or not
     // if not pass in an error message to the field
     // if all fields is set corretly display success message and clear all the fields
-    
+
     event.preventDefault();
     Object.keys(data).forEach((name) => {
       handleFormChange(name, data[name].value);
@@ -82,89 +82,96 @@ function App() {
     Object.keys(data).forEach((name) => {
       const error = data[name].error;
 
-      if (error !== '') {
+      if (error !== "") {
         status = false;
       }
     });
     if (status) {
       setSuccess(true);
       setData({
-        fname: { value: '', error: '' },
-        lname: { value: '', error: '' },
-        email: { value: '', error: '' },
-        qType: { value: '', error: '' },
-        message: { value: '', error: '' },
-        consent: { value: false, error: '' },
+        fname: { value: "", error: "" },
+        lname: { value: "", error: "" },
+        email: { value: "", error: "" },
+        qType: { value: "", error: "" },
+        message: { value: "", error: "" },
+        consent: { value: false, error: "" },
       });
     }
   }
   return (
-    <div className='font-karla bg-green-200 text-grey-900 text-base min-h-screen flex justify-center items-center relative'>
+    <div className="font-karla bg-green-200 text-grey-900 text-base min-h-screen flex justify-center items-center relative">
       {success && <Success />}
-      <main className='bg-white rounded-xl my-5 p-5 w-9/10 max-w-lg'>
+      <main className="bg-white rounded-xl my-5 p-5 w-9/10 max-w-lg md:max-w-xl">
         <form>
-          <h1 className='text-3xl font-bold text-grey-900 mb-2'>Contact Us</h1>
-          <Label name={'fname'}>First Name</Label>
-          <Input
-            name='fname'
-            onChange={handleFormChange}
-            error={data.fname.error}
-          />
+          <h1 className="text-3xl font-bold text-grey-900 mb-2">Contact Us</h1>
+          <div className="md:flex md:gap-2">
+            <legend className="grow">
+              <Label name={"fname"}>First Name</Label>
+              <Input
+                name="fname"
+                onChange={handleFormChange}
+                error={data.fname.error}
+              />
+            </legend>
 
-          <Label name={'lname'}>Last Name</Label>
-          <Input
-            name='lname'
-            onChange={handleFormChange}
-            error={data.lname.error}
-          />
+            <legend className="grow">
+              <Label name={"lname"}>Last Name</Label>
+              <Input
+                className="grow"
+                name="lname"
+                onChange={handleFormChange}
+                error={data.lname.error}
+              />
+            </legend>
+          </div>
 
-          <Label name={'email'}>Email Address</Label>
+          <Label name={"email"}>Email Address</Label>
           <Input
-            name='email'
-            type='email'
+            name="email"
+            type="email"
             onChange={handleFormChange}
             error={data.email.error}
           />
 
           <Label>Query Type</Label>
-          <Radio name='qType' value={'general'} onChange={handleFormChange}>
+          <Radio name="qType" value={"general"} onChange={handleFormChange}>
             General Enquiry
           </Radio>
-          <Radio name='qType' value={'support'} onChange={handleFormChange}>
+          <Radio name="qType" value={"support"} onChange={handleFormChange}>
             Support Request
           </Radio>
           <Error message={data.qType.error} />
 
-          <Label name='message'>Message</Label>
+          <Label name="message">Message</Label>
           <textarea
-            name='message'
-            id='message'
-            onChange={(e) => handleFormChange('message', e.target.value)}
-            className='border border-grey-500 rounded-sm block my-2 w-full h-40  focus:outline-none focus:border-green-600 resize-none'
+            name="message"
+            id="message"
+            onChange={(e) => handleFormChange("message", e.target.value)}
+            className="border border-grey-500 rounded-sm block my-2 w-full h-40  focus:outline-none focus:border-green-600 resize-none"
           />
           <Error message={data.message.error} />
 
-          <div className='flex items-center mt-10'>
+          <div className="flex items-center mt-10">
             <input
-              type='checkbox'
-              name='consent'
-              id='consent'
-              className='hover:cursor-pointer accent-green-600 '
-              onClick={() => handleFormChange('consent', !data.consent)}
+              type="checkbox"
+              name="consent"
+              id="consent"
+              className="hover:cursor-pointer accent-green-600 "
+              onClick={() => handleFormChange("consent", !data.consent)}
             />
             <label
-              htmlFor='consent'
-              className='inline-block ml-3 hover:cursor-pointer'
+              htmlFor="consent"
+              className="inline-block ml-3 hover:cursor-pointer"
             >
-              I consent to being contacted by the team{' '}
-              <span className='text-green-600'>*</span>
+              I consent to being contacted by the team{" "}
+              <span className="text-green-600">*</span>
             </label>
           </div>
           <Error message={data.consent.error} />
           <button
-            type='button'
+            type="button"
             onClick={handleSubmit}
-            className='w-full text-white bg-green-600 py-3 rounded-md mt-10 cursor-pointer'
+            className="w-full text-white bg-green-600 py-3 rounded-md mt-10 cursor-pointer"
           >
             Submit
           </button>
